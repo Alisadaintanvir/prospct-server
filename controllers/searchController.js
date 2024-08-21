4f826982622288a74d52ed4eb2cb9b2ae3947e8a
Alisadaintanvir <alisadaintanvir@gmail.com>
2024-08-21 13:53:57 +0600

    add exclusions and filters

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,4 +1,5 @@
 const { client } = require("../config/db");
+const parseNumber = require("../utils/parseNumber");
 
 const searchController = {
   search: async (req, res) => {
@@ -6,12 +7,13 @@ const searchController = {
       const db = client.db();
       const collection = db.collection("contacts_v5");
 
-      const filters = req.body;
+      const { filters, excludedFilters } = req.body;
       const query = {};
       const limit = filters.limit || 5;
       const page = filters.currentPage || 1;
 
       const conditions = [];
+      const exclusionConditions = [];
 
       // Country Filter
       if (filters.countries && filters.countries.length > 0) {
@@ -20,15 +22,42 @@ const searchController = {
         });
       }
 
+      // Exclusion Country Filters
+      if (excludedFilters.countries && excludedFilters.countries.length > 0) {
+        exclusionConditions.push({
+          "_source.person_location_country": {
+            $nin: excludedFilters.countries,
+          },
+        });
+      }
+
       // Job Title Filter
       if (filters.jobTitle && filters.jobTitle.length > 0) {
         conditions.push({ "_source.person_title": { $in: filters.jobTitle } });
+      }
+
+      // Exclusion Job Filters
+      if (excludedFilters.jobTitle && excludedFilters.jobTitle.length > 0) {
+        exclusionConditions.push({
+          "_source.person_title": {
+            $nin: excludedFilters.jobTitle,
+          },
+        });
       }
 
       // Seniority Filter
       if (filters.seniority && filters.seniority.length > 0) {
         conditions.push({
           "_source.person_seniority": { $in: filters.seniority },
+        });
+      }
+
+      // Exclusion Seniority Filters
+      if (excludedFilters.seniority && excludedFilters.seniority.length > 0) {
+        exclusionConditions.push({
+          "_source.person_seniority": {
+            $nin: excludedFilters.seniority,
+          },
         });
       }
 
@@ -39,9 +68,13 @@ const searchController = {
         });
       }
 
-      // Gender Filter
-      if (filters.gender && filters.gender.length > 0) {
-        conditions.push({ "_source.person_gender": { $in: filters.gender } });
+      // Exclusion Industry Filters
+      if (excludedFilters.industry && excludedFilters.industry.length > 0) {
+        exclusionConditions.push({
+          "_source.organization_industries": {
+            $nin: excludedFilters.industry,
+          },
+        });
       }
 
       // Email Status Filter
@@ -51,9 +84,30 @@ const searchController = {
         });
       }
 
+      // Exclusion Email Status Filters
+      if (
+        excludedFilters.emailStatus &&
+        excludedFilters.emailStatus.length > 0
+      ) {
+        exclusionConditions.push({
+          "_source.person_email_status_cd": {
+            $nin: excludedFilters.emailStatus,
+          },
+        });
+      }
+
       // Person Name Filter
       if (filters.personName && filters.personName.length > 0) {
         conditions.push({ "_source.person_name": { $in: filters.personName } });
+      }
+
+      // Exclusion Person Name Filters
+      if (excludedFilters.personName && excludedFilters.personName.length > 0) {
+        exclusionConditions.push({
+          "_source.person_name": {
+            $nin: excludedFilters.personName,
+          },
+        });
       }
 
       // Employee Range Filter
@@ -81,6 +135,47 @@ const searchController = {
         }
       }
 
+      // Employee Range Exclusion Filter
+      if (
+        excludedFilters.employeeRange &&
+        excludedFilters.employeeRange.length > 0
+      ) {
+        const excludedEmployeeRanges = excludedFilters.employeeRange
+          .map((range) => {
+            const normalizedRange = range.replace(/and/g, "-");
+            const [minStr, maxStr] = normalizedRange
+              .split("-")
+              .map((str) => str.trim());
+
+            const min = parseNumber(minStr);
+            const max = parseNumber(maxStr);
+            if (isNaN(min)) return null;
+            if (isNaN(max)) {
+              return {
+                $or: [
+                  {
+                    "_source.organization_num_current_employees": { $lt: min },
+                  },
+                  {
+                    "_source.organization_num_current_employees": { $gt: min },
+                  },
+                ],
+              };
+            }
+            return {
+              $or: [
+                { "_source.organization_num_current_employees": { $lt: min } },
+                { "_source.organization_num_current_employees": { $gt: max } },
+              ],
+            };
+          })
+          .filter(Boolean);
+
+        if (excludedEmployeeRanges.length > 0) {
+          exclusionConditions.push({ $or: excludedEmployeeRanges });
+        }
+      }
+
       // Revenue Range Filter
       if (filters.revenueRange && filters.revenueRange.length > 0) {
         const revenueRanges = filters.revenueRange
@@ -89,6 +184,9 @@ const searchController = {
             const [minStr, maxStr] = normalizedRange
               .split("-")
               .map((str) => str.trim());
+
+            // Remove 'M' and convert to a number in thousands
+
             const min = parseNumber(minStr);
             const max = parseNumber(maxStr);
             if (isNaN(min)) return null;
@@ -110,9 +208,51 @@ const searchController = {
         }
       }
 
+      // Revenue Range Filter and Exclusion (same as before)
+      if (filters.revenueRange && filters.revenueRange.length > 0) {
+        const revenueRanges = filters.revenueRange
+          .map((range) => {
+            const normalizedRange = range.replace(/and/g, "-");
+            const [minStr, maxStr] = normalizedRange
+              .split("-")
+              .map((str) => str.trim());
+
+            const min = parseNumber(minStr);
+            const max = parseNumber(maxStr);
+            if (isNaN(min)) return null;
+            if (isNaN(max)) {
+              return {
+                "_source.organization_revenue_in_thousands_int": { $gte: min },
+              };
+            }
+            return {
+              "_source.organization_revenue_in_thousands_int": {
+                $gte: min,
+                $lte: max,
+              },
+            };
+          })
+          .filter(Boolean);
+        if (revenueRanges.length > 0) {
+          conditions.push({ $or: revenueRanges });
+        }
+      }
+
+      // Email Type Filter
+      if (filters.emailType && filters.emailType.length > 0) {
+        const emailDomainRegex = new RegExp(`@${filters.emailDomain}$`, "i");
+        conditions.push({
+          "_source.person_email": { $regex: emailDomainRegex },
+        });
+      }
+
       // Apply all conditions
       if (conditions.length > 0) {
         query.$and = conditions;
+      }
+
+      if (exclusionConditions.length > 0) {
+        query.$and = [...(query.$and || []), ...exclusionConditions];
       }
 
       const contacts = await collection
